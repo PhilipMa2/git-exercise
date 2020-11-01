49663d0d091918df6dc8e8ff7e442f9ec54ccf2a
PhilipMa2 <ma_philip2000@hotmail.com>
2020-11-01 16:38:43 -0600

    Update HelloWorld.java

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 public class HelloWorld {
   public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    pen.println ("Hello, GitHub!");
+    pen.println ("Hello, GitHub again!");
     pen.flush();
   } // main(String[])
 }
